9010be9f387c1a4d5457804eb97efaae873f21c4
PatrykFlama <patrykflama@interia.pl>
2022-12-09 20:15:01 +0100

    feature: time

--- src/terminal_game_test.cpp
+++ src/terminal_game_test.cpp
@@ -3,9 +3,53 @@
 // TODO: (in distant future) separate main file to separate ones - probably struct per file + headers + main
 
 
+struct Time{
+    int framerate;      // !frames per second
+    int time_start;
+    int last_block_movement;    // this probably should be in game structure, but at this point in time it is nonexistent
+    int last_frame;
+
+    Time() : Time(30, 1000){}
+    Time(int _framerate, int game_start_delay) : framerate(_framerate), time_start(time()){
+        last_block_movement = time() + game_start_delay;
+        last_frame = 0;
+    }
+
+    int time() {
+        return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
+    }
+
+    int ticks(){
+        return ((time()-time_start)*framerate)/1000;
+    }
+
+    int restart_time(){
+        time_start = time();
+    }
+
+    bool next_frame(){
+        if(last_frame < ticks()){
+            last_frame = ticks();
+            cout << "\n----" << ticks() << "----\n";
+            return true;
+        }
+        return false;
+    }
+
+    bool should_block_fall(int blocks_falling_speed){    // this probably should be in game structure, but at this point in time it is nonexistent
+        if(blocks_falling_speed > time() - last_block_movement){
+            last_block_movement += blocks_falling_speed;
+            return true;
+        }
+        return false;
+    }
+};
+
+
 struct Board{
     int HEIGHT, WIDTH;
-    int game_state = 0;         //? 0 -> game in progress, 1 -> game won, -1 -> game lost
+    int game_state;         //? 0 -> game in progress, 1 -> game won, -1 -> game lost
+    int blocks_falling_speed;   // in milliseconds
     #define board_type char
     /*board variables:
       -> nothing
@@ -18,12 +62,15 @@ struct Board{
     vector< vector<board_type> > board;        //? [row/height][column/width]; enumerated from 0
 
     // __init__
-    Board() : Board(10, 20){}
+    Board() : Board(10, 20, 1000){}
 
-    Board(int _HEIGHT, int _WIDTH){
+    Board(int _HEIGHT, int _WIDTH, int _blocks_falling_speed){
         HEIGHT = _HEIGHT;
         WIDTH = _WIDTH;
         board.resize(HEIGHT, vector<board_type>(WIDTH, ' '));
+
+        game_state = 0;
+        blocks_falling_speed = _blocks_falling_speed;
     }
 
     void draw_board(){
@@ -94,6 +141,9 @@ struct Player{
         
     }
 
+    void get_input(){}
+
+    void interpret_input(){}
 };
 
 
@@ -103,20 +153,32 @@ struct Game{
 
 
 int main(){
+    Time time;
     Board board;
     Player player_1;
     // Player player_2;
     const bool game_is_on = true;
+    time.framerate = 1;
 
     while(game_is_on){
+        // TODO: some menu things
+        // TODO: when game start option has been chosen
+            time.restart_time();
+
         while(board.game_state == 0){       // while game is beeing played
             // TODO: get input
             // TODO: make move (if possible)
             board.draw_board();
 
-            // TODO: check for game falling tick
-                // TODO: fall the blocks (if possible)
-                // TODO: redraw board to prevent visual action lag
+            if(time.should_block_fall(board.blocks_falling_speed)){
+                board.block_fall();
+                // TODO: check for game over
+                // TODO: score
+                board.draw_board();
+            }
+
+            while(time.next_frame()){}      // wait for next frame TODO: catch user input
+            int trash; cin >> trash;
         }
         // TODO: check result and make some end_screen
     }
