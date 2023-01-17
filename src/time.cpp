953051d9e33adda20c0de962662823a454e0111c
Tomasz Żyła <tomasz.zyla.xyz@gmail.com>
2023-01-17 21:16:52 +0100

    time update #3

--- src/time.cpp
+++ src/time.cpp
@@ -1,30 +1,60 @@
-#include "Time.h"
-#include <gameplay.h>
+#include "time.h"
 #include <chrono>
-#include <time.h>
+
+using namespace std::chrono_literals;
+
+/*
+    Level         Drop speed (frames/line)
+     00            48 (0.8 s)
+     01            43 (0.72s)
+     02            38 (0.63s) 
+     03            33 (0.55s) 
+     04            28 (0.47s) 
+     05            23 (0.38s)
+     06            18 (0.3 s) 
+     07            13 (0.22s)
+     08             8 (0.13s)
+     09             6 (0.1 s)
+     10-12          5 (0.08s) 
+     13-15          4 (0.07s)
+     16-18          3 (0.05s)
+     19-28          2 (0.03s)
+     29+            1 (0.02s)
+    */
 
 const int M = 1e7;
 
 int Time::getSystemTime() {
-	int systemTime = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count()) % M;
+	int systemTime = std::chrono::duration_cast<milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() % M;
 	return systemTime;
 }
 
-Time::Time(int _framerate, int _time_start, int game_start_delay) : framerate(_framerate), time_start(getSystemTime()){
-	last_block_movement = time() + game_start_delay;
+int Time::convertTime(milliseconds T) {
+	int convertedT = std::chrono::duration_cast<milliseconds>(T).count() % M;
+	return convertedT;
 }
 
-int Time::getLastDropTime() {
-	// TODO
+Time::Time(int _framerate, milliseconds _game_start_delay, int _difficulty_level) : framerate(_framerate), time_start(getSystemTime()), game_start_delay(_game_start_delay){
+	last_block_movement = getSystemTime() + convertTime(_game_start_delay);
+	block_drop_speed[0] = 800ms, block_drop_speed[1] = 720ms, block_drop_speed[2] = 630ms, block_drop_speed[3] = 550ms, block_drop_speed[4] = 470ms, 
+	block_drop_speed[5] = 380ms, block_drop_speed[6] = 300ms, block_drop_speed[7] = 220ms, block_drop_speed[8] = 130ms, block_drop_speed[9] = 100ms,
+	block_drop_speed[10] = 80ms, block_drop_speed[11] = 80ms, block_drop_speed[12] = 80ms, block_drop_speed[13] = 70ms, block_drop_speed[14] = 70ms,
+	block_drop_speed[15] = 70ms, block_drop_speed[16] = 50ms, block_drop_speed[17] = 50ms, block_drop_speed[18] = 50ms, block_drop_speed[29] = 20ms;
+	for (uint level = 19; level <= 29; level ++) block_drop_speed[level] = 30ms;
+}
+
+int Time::getLastBlockMovement() {
+	return last_block_movement;
 }
 
 int Time::getDropSpeed() {
-	// TODO
+	if (difficulty_level > 29) return convertTime(block_drop_speed[29]);
+	return convertTime(block_drop_speed[difficulty_level]);
 }
 
-bool should_block_fall(int blocks_falling_speed){
-	if(blocks_falling_speed < getSystemTime() - last_block_movement){
-		last_block_movement += blocks_falling_speed;
+bool Time::tick() {
+	if (getSystemTime() - getLastBlockMovement() >= getDropSpeed()) {
+		last_block_movement = getSystemTime();
 		return true;
 	}
 	return false;
--- src/time.h
+++ src/time.h
@@ -1,37 +1,30 @@
 #ifndef TIME_H
 #define TIME_H
-#include <time.h>
+#include <chrono>
+#include <vector>
+
+#define BLOCKDROPSPEEDSIZE 30
+typedef std::chrono::milliseconds milliseconds;
+typedef unsigned int uint;
 
 class Time {
+private:
     int framerate;
     int time_start;
-    int game_start_delay;
+    milliseconds game_start_delay;
     int last_block_movement;
+    int difficulty_level;
+    std::vector<milliseconds> block_drop_speed;
+    int getSystemTime();
+    int getLastDropTime();
+    int getDropSpeed();
+    int getLastBlockMovement();
     
 public:
-	/*
-    Level         Drop speed (frames/line)
-     00            48 (0.8 s)
-     01            43 (0.72s)
-     02            38 (0.63s) 
-     03            33 (0.55s) 
-     04            28 (0.47s) 
-     05            23 (0.38s)
-     06            18 (0.3 s) 
-     07            13 (0.22s)
-     08             8 (0.13s)
-     09             6 (0.1 s)
-     10-12          5 (0.08s) 
-     13-15          4 (0.07s)
-     16-18          3 (0.05s)
-     19-28          2 (0.03s)
-     29+            1 (0.02s)
-    */
-    Time(int _framerate, int _time_start, int _game_start_delay);
-	int getSystemTime();
-	int getLastDropTime();
-	//int getDropSpeed();
-	bool should_block_fall(int _blocks_falling_speed);
+	Time(): block_drop_speed(BLOCKDROPSPEEDSIZE) {}
+    Time(int _framerate, milliseconds _game_start_delay, int _difficulty_level);
+	int convertTime(milliseconds _T);
+	bool tick();
 };
 
 #endif
